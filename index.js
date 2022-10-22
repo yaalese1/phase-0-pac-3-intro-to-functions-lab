2a6286216a2423021d8cee6b55710ce693707b7e
Yetunde Alese <yaalese1@gmail.com>
2022-10-22 17:04:06 -0400

    commit tree 358d4fb3b1f33cd80863573502d596f2846dbc16

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function shout(string){
+    //todo
+}
+function shout(string){
+    return string.toUpperCase();
+}
+function wishper(string){
+    //todo
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+
+
+    function logShout(string){
+        console.log(string.toUpperCase())
+    }
+
+    function logWhisper(string){
+        console.log(string.toLowerCase())
+    } 
+function sayHiToHeadphonedRoommate(string){
+    if(string.toLowerCase()===string)
+    return ("I can't hear you!")
+   
+    if (string.toUpperCase()===string)
+    return("YES INDEED!")
+
+    if ("Let's have dinner together!" === string)
+    return ("I would love to!")
+   
+}
+
+
+
+
+
+
+
+
+
